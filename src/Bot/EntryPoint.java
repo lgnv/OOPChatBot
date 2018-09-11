5e76602fb61945c6eecf0c656f0b42df8c16b290
Евгений <gekachan2000@gmail.com>
2018-09-11 21:47:10 +0500

    Исправление ошибки

---
+++ src/Bot/EntryPoint.java
@@ -0,0 +1,7 @@
+package Bot;
+
+public class EntryPoint {
+	public static void main(String[] args) {
+		System.out.println("Hello, user!");
+	}
+}
--- src/Bot/EnttyPoint.java
+++
@@ -1,5 +0,0 @@
-package Bot;
-
-public class EnttyPoint {
-
-}
