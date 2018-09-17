4771cc0e1d602e5a1f79b02d522421cb03d20d44
Евгений <gekachan2000@gmail.com>
2018-09-17 22:50:26 +0500

    Добавлена секретная функция - показ котика

--- src/First/TelegramBot.java
+++ src/First/TelegramBot.java
@@ -3,6 +3,8 @@ package First;
 import java.util.HashMap;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendAnimation;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -23,7 +25,16 @@ public class TelegramBot extends TelegramLongPollingBot {
 		updateUsers(userId);
 		var currentUser = users.get(userId);
 		currentUser.sendMessage(textFromUser);
-		replyToUser(userId, currentUser);
+		if (textFromUser.equalsIgnoreCase("cat")) {
+			sendGif(userId, "https://psv4.userapi.com/c834502/u140417658/"
+					+ "docs/d13/cfae75c3f120/stereotipy.gif?extra=Ygx2aCfu_Up"
+					+ "AUTB-45umcTNrU_OEicuPTQkUaKuNgJNRS-eFhY2ET4QPoKRMu2gKbP"
+					+ "MuHXyhIaWj6k-9VYlFqozTsrEZViO01TJp6CgofXHPD1lC0bFQOZ6uit"
+					+ "BgoTNMRhKC4pMOOlV-Yg");
+		}
+		else {
+			replyToUser(userId, currentUser);
+		}
 	}
 
 	private void replyToUser(Long userId, User currentUser) {
@@ -33,12 +44,23 @@ public class TelegramBot extends TelegramLongPollingBot {
 			}
 			var botMessage = new SendMessage();
 			botMessage.setChatId(userId);
-			botMessage.setText(textFromBot);
+			botMessage.setText(textFromBot); 
 			try {
 				execute(botMessage);
 			} catch (TelegramApiException e1) {
 				e1.printStackTrace();
 			}
+		}
+	}
+
+	private void sendGif(Long userId, String url) {
+		var gif = new SendDocument();
+		gif.setDocument(url);
+		gif.setChatId(userId);
+		try {
+			execute(gif);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
 		}
 	}
 
