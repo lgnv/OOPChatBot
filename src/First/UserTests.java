bd3d1ab5a582af0a2773b504eb4b61113e361135
Евгений <gekachan2000@gmail.com>
2018-09-18 09:40:45 +0500

    Добавлены тесты на класс User

---
+++ src/First/UserTests.java
@@ -0,0 +1,28 @@
+package First;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserTests {
+
+	@Test
+	void testSendMessage() {
+		var bot = new Bot();
+		var user = new User(0);
+		user.addListener(bot);
+		user.sendMessage("игры");
+		assertTrue(user.getReceivedFromBotMessages().size() > 0);
+	}
+	
+	@Test
+	void testWithoutListeners() {
+		var bot = new Bot();
+		var user = new User(0);
+		user.addListener(bot);
+		user.removeListener(bot);
+		user.sendMessage("message");
+		assertTrue(user.getReceivedFromBotMessages().size() == 0);
+	}
+
+}
